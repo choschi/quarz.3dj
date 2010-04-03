b3e7385c8e4f30f42775f64b7ac8856b793ad349
Sven Osterwalder <sosterwalder@slot6.row2.visitors.breakpoint-party.de>
2010-04-03 11:03:27 +0200

    Added error handling

--- Vector.js
+++ Vector.js
@@ -2,24 +2,52 @@
  * @author sosterwalder
  */
 
-function Vector(x, y, z, h) {
-	this.x = x;
-	this.y = y;
-	this.z = z; 
+function Vector(x, y, z) {
+	try {
+		this.x = x;
+		this.y = y;
+		this.z = z; 
+		this.h = 1;
+	}
+	catch (!_isInt(x)) {
+		throw "x has to be a number!";
+	}
+	catch (!_isInt(y)) {
+		throw "y has to be a number!";
+	}
+	catch (!_isInt(z)) {
+		throw "z has to be a number!";
+	}
 }
 
 function setX(x) {
-	this.x = x;
+	try {
+		this.x = x;
+	}
+	catch (!_isInt(x)) {
+		throw "x has to be a number!";
+	}
+		
 }
 Vector.prototype.setX = setX;
 
 function setY(y) {
-	this.y = y;
+	try {
+		this.y = y;
+	}
+	catch (!_isInt(y)) {
+		throw "y has to be a number!";
+	}
 }
 Vector.prototype.setY = setY;
 
 function setZ(z) {
-	this.z = z;
+	try {
+		this.z = z;
+	}
+	catch (!_isInt(z)) {
+		throw "z has to be a number!";
+	}
 }
 Vector.prototype.setZ = setZ;
 
@@ -42,3 +70,9 @@ function getH() {
 	return this.h;
 }
 Vector.prototype.getH = getH;
+
+function _isInt(i) {
+	var j = parseInt(i);
+	if (isNaN(j)) return false;
+	return i == j && i.toString() == j.toString();
+}
