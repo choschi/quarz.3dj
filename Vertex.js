e32d20e9811f6dd8878eef4e40ac534680ca4d0a
Sven Osterwalder <sosterwalder@slot6.row2.visitors.breakpoint-party.de>
2010-04-03 11:26:18 +0200

    Initial release of Vertex.js

---
+++ Vertex.js
@@ -0,0 +1,78 @@
+/**
+ * @author sosterwalder
+ */
+
+function Vertex(x, y, z) {
+	try {
+		this.x = x;
+		this.y = y;
+		this.z = z; 
+		this.h = 1;
+	}
+	catch (!_isInt(x)) {
+		throw "x has to be a number!";
+	}
+	catch (!_isInt(y)) {
+		throw "y has to be a number!";
+	}
+	catch (!_isInt(z)) {
+		throw "z has to be a number!";
+	}
+}
+
+function setX(x) {
+	try {
+		this.x = x;
+	}
+	catch (!_isInt(x)) {
+		throw "x has to be a number!";
+	}
+		
+}
+Vertex.prototype.setX = setX;
+
+function setY(y) {
+	try {
+		this.y = y;
+	}
+	catch (!_isInt(y)) {
+		throw "y has to be a number!";
+	}
+}
+Vertex.prototype.setY = setY;
+
+function setZ(z) {
+	try {
+		this.z = z;
+	}
+	catch (!_isInt(z)) {
+		throw "z has to be a number!";
+	}
+}
+Vertex.prototype.setZ = setZ;
+
+function getX() {
+	return this.x;
+}
+Vertex.prototype.getX = getX;
+
+function getY() {
+	return this.y;
+}
+Vertex.prototype.getY = getY;
+
+function getZ() {
+	return this.z;
+}
+Vertex.prototype.getZ = getZ;
+
+function getH() {
+	return this.h;
+}
+Vertex.prototype.getH = getH;
+
+function _isInt(i) {
+	var j = parseInt(i);
+	if (isNaN(j)) return false;
+	return i == j && i.toString() == j.toString();
+}
